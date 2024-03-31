77f73fbece9cbca22d9f42b2fecdc2ef011be97d
Abhinav Yadav <abhinavay2003@gmail.com>
2024-03-31 22:30:05 +0530

    Update search input and search results styling

--- src/root/pages/Search.jsx
+++ src/root/pages/Search.jsx
@@ -25,11 +25,11 @@ const Search = () => {
   return (
     <section className="">
       <div className="max-w-4xl w-full mx-auto">
-        <div className="w-full pb-3 bg-white pt-[120px] max-w-4xl fixed mx-auto right-0 left-0">
+        <div className="w-full pb-3 bg-black pt-[120px] max-w-4xl fixed mx-auto right-0 left-0">
           <div className="relative">
-          <input className="w-full border border-gray-700 outline-none pl-3 pr-10 py-2 rounded-md text-xl text-gray-600 " type="text" placeholder="Search for restaurants and food..." value={searchQuery} onChange={handleSetSearchQuery} />
+          <input className="w-full border border-gray-700 outline-none pl-3 pr-10 py-2 rounded-md text-xl text-white placeholder:text-gray-300 bg-slate-950  " type="text" placeholder="Search for restaurants and food..." value={searchQuery} onChange={handleSetSearchQuery} />
           {
-            searchQuery === "" ? <IoSearchOutline className="absolute h-full text-gray-600 right-3 top-0 text-3xl" /> : <button className="absolute text-gray-600 right-3 top-2 text-3xl" onClick={handleClearSearchQuery} > <RxCross1 /> </button>
+            searchQuery === "" ? <IoSearchOutline className="absolute h-full text-gray-300 right-3 top-0 text-3xl" /> : <button className="absolute text-gray-300 right-3 top-2 text-3xl" onClick={handleClearSearchQuery} > <RxCross1 /> </button>
           }
           </div>
         </div>
@@ -38,11 +38,11 @@ const Search = () => {
           {
             searchData?.map((data) => {
               return (
-                <Link key={uuidv4()} to={"/"} className="flex gap-2 w-full hover:bg-blue-50 p-3 rounded-lg">
+                <Link key={uuidv4()} to={"/"} className="flex gap-2 w-full hover:bg-slate-950 p-3 border border-transparent hover:border hover:border-slate-500 rounded-lg">
                   <div><img loading="lazy" className="w-[60px] rounded-md" src={IMG_SEARCH_URL + data.cloudinaryId} alt={data.metadata} /></div>
                   <div className="flex flex-col">
-                    <h3 className="text-gray-700 text-sm">{data.text}</h3>
-                    <p className="text-gray-500 font-light text-xs">{data.type}</p>
+                    <h3 className="text-gray-100 text-sm">{data.text}</h3>
+                    <p className="text-gray-300 font-light text-xs">{data.type}</p>
                   </div>
                 </Link>
                 )
@@ -54,7 +54,7 @@ const Search = () => {
         {
           isLoading && searchData?.length === 0 && (
             <div className="pt-[200px] flex justify-center mx-auto">
-              <TbLoader3 className="animate-spin text-3xl text-gray-500 " />
+              <TbLoader3 className="animate-spin text-3xl text-gray-300 " />
             </div>
           )
         }
